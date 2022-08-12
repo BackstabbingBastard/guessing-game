01e18bb4d0647d1792cb254691189110b7ad8348
Andrey Zaderenko <andrey42work@gmail.com>
2022-08-12 11:03:45 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = (this.max + this.min) / 2;
+        this.mid = Math.round(this.mid);
+        return this.mid;
     }
 
     lower() {
+        // this.max = this.mid + 1;
+        this.max = this.mid;
 
     }
 
     greater() {
+        // this.min = this.mid - 1;
+        this.min = this.mid;
 
     }
 }
